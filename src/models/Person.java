f456fc7b7914e84fe3dc45740931c9687b4313d7
miraseren <153474665+miraseren@users.noreply.github.com>
2024-01-29 02:30:33 +0600

    Person.java

--- src/models/Person.java
+++ src/models/Person.java
@@ -2,47 +2,44 @@ package models;
 
 import interfaces.Payable;
 
-public abstract class Person implements Payable, Comparable<Person> {
-    private static int idCounter = 1;
-    private int id;
-    private String name = "Name";
-    private String surname = "Surname";
+public abstract class Person implements Payable, Comparable<Person>{ // creating abstract class that implement two interfaces/
+    static int idCounter = 1; // creating static variable
+    int id; // id variable
+    String name, surname; // representing name and surname
 
-    public Person() {
-        this("Name", "Surname");
+    public Person(){ // declaring constructor
+        id = idCounter++; // incrementing id value
+        name = "Name"; //value of name
+        surname = "Surname"; //value of surname
+    }
+    public Person(String name, String surname){ // declaring constructor
+        id = idCounter++; //incrementing id value
+        this.name = name; // assigning the values
+        this.surname = surname; // assigning the values
     }
 
-    public Person(String name, String surname) {
-        this.id = idCounter++;
-        this.name = name;
+    public void setName(String name) { // setter method
+        this.name = name; //
+    }
+    public void setSurname(String surname){ //setter method
         this.surname = surname;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getId() {
+    public int getId(){ //getter method
         return id;
     }
-
-    public String getName() {
+    public String getName(){ // getter method
         return name;
     }
-
-    public String getSurname() {
+    public String getSurname(){ //getter method
         return surname;
     }
 
-    @Override public String toString(){
-        return id + ". " + name + " " + surname;
+    @Override
+    public String toString(){ //method to obtian String representation
+        return id + ". " + name + " " + surname; // concatenating id name surname
     }
-    public abstract String getPosition();
-    public String printData(){
-        return toString() + " earns " + getPaymentAmount() + " tenge.";
+    public abstract String getPosition();   // declaring abstract method
+    public String printData(){ // declaring method that prints Data
+        return toString() + " earns " + getPaymentAmount() + " tenge."; // returns concatenation two methods
     }
 }
