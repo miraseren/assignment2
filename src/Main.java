c3c73e529b0c97f7f3105722c70cf282e56fd8ad
miraseren <153474665+miraseren@users.noreply.github.com>
2024-01-28 17:19:11 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,29 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Person e1 = new Employee("Miras", "Yeren", "Lawyer", 5420);
-        Person e2 = new Employee("Ilyas", "Zhaksybayev", "Manager", 4200);
-        Person e3 = new Employee("Olzhas", "Karabekov", "Jugde", 1150000);
-        Person e4 = new Employee("Dias", "Altynov", "Programmer", 163450);
-        Person e5 = new Employee("Nurasyl", "Khurmet", "Armwrestler", 360500);
+        Person m1 = new Employee("Miras", "Yeren", "Lawyer", 19000);
+        Person m2 = new Employee("Ilyas", "Zhaksybayev", "Manager", 360);
+        Person m3 = new Employee("Olzhas", "Karabekov", "Jugde", 1000000);
+        Person m4 = new Employee("Dias", "Altynov", "Programmer", 14200);
+        Person m5 = new Employee("Nurasyl", "Khurmet", "Armwrestler", 777777);
 
-        Person s1 = new Student("Kazakh", "Messi", 2.81);
-        Person s2 = new Student("Russian", "Mbappe", 2.17);
-        Person s3 = new Student("Sponge", "Bob", 2.02);
-        Person s4 = new Student("Alken", "Zaiyr", 3.14);
-        Person s5 = new Student("Bekzhan", "Chan", 3.83);
+        Person p1 = new Student("Kazakh", "Messi", 1.17);
+        Person p2 = new Student("Russian", "Mbappe", 0.21);
+        Person p3 = new Student("Patrick", "Bob", 3.05);
+        Person p4 = new Student("Alken", "Zaiyr", 2.00);
+        Person p5 = new Student("Bekzhan", "Chan", 3.83);
 
         ArrayList<Person> persons = new ArrayList<>();
-        persons.add(e1);
-        persons.add(e2);
-        persons.add(e3);
-        persons.add(e4);
-        persons.add(e5);
-        persons.add(s1);
-        persons.add(s2);
-        persons.add(s3);
-        persons.add(s4);
-        persons.add(s5);
+        persons.add(m1);
+        persons.add(m2);
+        persons.add(m3);
+        persons.add(m4);
+        persons.add(m5);
+        persons.add(p1);
+        persons.add(p2);
+        persons.add(p3);
+        persons.add(p4);
+        persons.add(p5);
 
         Collections.sort(persons);
         printInformation(persons);
